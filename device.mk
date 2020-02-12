94dc741d8aad533d7cfdbba33d3de3da18fa84c2
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    RMX1921: Enable freeform windows
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     RealmeParts
 
+Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710 \
