194b094b64c19a2a06f718337dbf8b02b1bdca65
improbir <ghosh_probir@yahoo.com>
2020-07-22 17:09:26 +0000

    RMX1921: Nuke all unnecessary props
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -1,9 +1,3 @@
-#
-# Copyright (C) 2018-2019 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Audio
 PRODUCT_PROPERTY_OVERRIDES +=  \
     af.fast_track_multiplier=1 \
@@ -85,8 +79,9 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     vendor.audio.volume.headset.gain.depcal=true \
     vendor.audio_hal.period_size=192
 
+
 # Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PROPERTY_OVERRIDES +=  \
     vendor.qcom.bluetooth.soc=cherokee
 
 # Camera
@@ -98,35 +93,22 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
    debug.stagefright.omx_default_rank.sw-audio=1 \
    debug.stagefright.omx_default_rank=0
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=384m \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=16m
-
 # Display
 PRODUCT_PROPERTY_OVERRIDES +=  \
     vendor.display.enable_default_color_mode=1 \
     ro.vendor.display.cabl=2 \
     ro.sf.lcd_density=420
 
-# Display density
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=420
-
 # DRM
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PROPERTY_OVERRIDES +=  \
     drm.service.enabled=true
 
 # FRP
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PROPERTY_OVERRIDES +=  \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
 
 # Graphics
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PROPERTY_OVERRIDES +=  \
     debug.egl.callstack=1 \
     debug.egl.hw=0 \
     debug.mdpcomp.logs=0 \
@@ -137,6 +119,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     vendor.gralloc.disable_ubwc=0
 
+# IMS
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.radio.data_con_rprt=1
+
 # LMKD
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.low_ram=false \
@@ -145,51 +134,22 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.lmk.use_minfree_levels=true \
     ro.lmk.log_stats=true
 
-# SurfaceFlinger
+# Perf
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
+# Surfaceflinger properties
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.wcg_composition_dataspace=143261696 \
     ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=2 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-
-# IMS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    persist.dbg.ims_volte_enable=1 \
-    persist.vendor.radio.data_ltd_sys_ind=1 \
-    persist.vendor.radio.data_con_rprt=1
-
-# Keystore
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.keystore_desede=true
-
-# Perf
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.at_library=libqti-at.so \
-    persist.vendor.qti.games.gt.prof=1
-
-# Qualcomm System Daemon
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qcomsysd.enabled=1
-
-# Radio
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.multisim.config=dsds \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1 \
-    persist.sys.fflag.override.settings_network_and_internet_v2=true \
-    persist.vendor.radio.aosp_usr_pref_sel=true
-
+    
 #AnxCam
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.vendor.device=RMX1921
