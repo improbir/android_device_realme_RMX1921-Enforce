09ac0d5c47bb90df386b0797fb952469767ddcc0
improbir <ghosh_probir@yahoo.com>
2020-08-20 18:15:49 +0000

    RMX1921: Fix 'internal problem' error pop up
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -150,6 +150,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
     
+# Treble
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    ro.treble.enabled=false
+
 #AnxCam
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.vendor.device=RMX1921
