30e96fa217743fb3300892d96d15a2c74f347984
Vachounet <vachounet@live.fr>
2020-09-13 04:21:00 +0000

    RMX1921: set BOARD_USES_QCNE
    Change-Id: Ifd3fd6db080c17a4fcf0cbf59f216bcbc5cd9476
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,9 @@ TARGET_HAS_WIDE_COLOR_DISPLAY := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# CNE / DPM
+BOARD_USES_QCNE := true
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
--- device.mk
+++ device.mk
@@ -105,6 +105,12 @@ PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0-impl.generic \
     android.hardware.contexthub@1.0-service
     
+# CNE
+PRODUCT_PACKAGES += \
+    cneapiclient \
+    com.quicinc.cne \
+    services-ext
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
