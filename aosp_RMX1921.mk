cace4441a0904297bc4d756d68c5ae3f12e9c7d2
improbir <ghosh_probir@yahoo.com>
2020-10-13 11:15:09 +0000

    RMX1921: Update coral FP to October SP
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- aosp_RMX1921.mk
+++ aosp_RMX1921.mk
@@ -30,10 +30,10 @@ PRODUCT_MANUFACTURER := Realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 11 RP1A.200720.009 6720564 release-keys" \
+    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201005.004 6782484 release-keys" \
     PRODUCT_NAME="RMX1921" \
     TARGET_DEVICE="RMX1921"
     
